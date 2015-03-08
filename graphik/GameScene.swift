f4f5dc8e1009e0dbe4d1b0cc1556af4a1fc48c6d
Zuzana Dostalova <dostalovaz@gmail.com>
2015-03-08 18:12:44 +0100

    BFS and textures

--- graphik/GameScene.swift
+++ graphik/GameScene.swift
@@ -8,7 +8,15 @@
 
 import SpriteKit
 
+class Tile: SKShapeNode {
+    var row: Int = 0
+    var col: Int = 0
+    var visited = false
+}
+
 class GameScene: SKScene {
+    var matrix = Array<Array<Tile>>()
+    
     override func didMoveToView(view: SKView) {
      
         let totalWidth = UIScreen.mainScreen().bounds.width
@@ -19,45 +27,93 @@ class GameScene: SKScene {
         let size: CGFloat = 30
         
         for var i = 0; i < rows; i = i + 1 {
+            var row = Array<Tile>()
             for var j = 0; j < cols; j++ {
                 let position = CGPoint(x: offset*j, y: offset*i)
                 
-                var color = SKColor.redColor()
-                if ((i + j) % 2 == 1) {
-                    color = SKColor.purpleColor()
+                let color = SKColor(red: 0.7, green: 0.45, blue: 0.65, alpha: 1)
+                let tile = generateSquare(position, size: size, color: color)
+                tile.row = i
+                tile.col = j
+
+                if arc4random() % 3 >= 2 {
+                    tile.visited = true
+                    tile.fillColor = SKColor(red: 0.4, green: 0.7, blue: 0.4, alpha: 1)
                 }
-                let square = generateSquare(position, size: size, color: color)
+
                 
-                self.addChild(square)
+                self.addChild(tile)
+                row.append(tile)
             }
+            
+            matrix.append(row)
         }
     }
     
-    func generateSquare(position: CGPoint, size: CGFloat, color: SKColor) -> SKShapeNode {
-        var square = SKShapeNode(rectOfSize: CGSize(width: size, height: size))
+    func generateSquare(position: CGPoint, size: CGFloat, color: SKColor) -> Tile {
+        var square = Tile(rectOfSize: CGSize(width: size, height: size))
         square.name = "square"
         square.fillColor = color
+        // square.fillTexture = SKTexture(imageNamed: "green2")
         square.position = position
         return square
     }
     
+    var queue = Array<Tile>()
+    
+    func bfs(tile: Tile?) {
+        if tile != nil {
+            queue.append(tile!)
+        }
+        
+        if (queue.count > 0) {
+            var tile = queue.removeAtIndex(queue.count - 1)
+//            var tile = queue.removeAtIndex(0)
+            
+            if (!tile.visited) {
+                tile.visited = true
+                tile.fillColor = SKColor(red: 0.5, green: 0.2, blue: 0.5, alpha: 1)
+                
+                queue.append(matrix[(tile.row) % matrix.count][(tile.col + 1) % matrix[0].count])
+                queue.append(matrix[(tile.row + 1) % matrix.count][(tile.col) % matrix[0].count])
+                
+                queue.append(matrix[tile.row][max(tile.col - 1, 0)])
+                queue.append(matrix[max(tile.row - 1, 0)][tile.col])
+                NSLog("visited tile %d %d", tile.row, tile.col)
+            }
+            
+            runAction(SKAction.sequence([
+                SKAction.waitForDuration(0.02),
+                SKAction.runBlock({
+                    self.bfs(nil)
+                })
+            ]))
+        }
+    }
+    
+    var started = false
+    
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         if let touch = touches.anyObject() as? UITouch {
-            let fadeOut = SKAction.fadeOutWithDuration(0)
             
-            if let shapeNode = nodeAtPoint(touch.locationInNode(self)) as? SKShapeNode {
-                shapeNode.runAction(fadeOut)
+            if let tile = nodeAtPoint(touch.locationInNode(self)) as? Tile {
+                if !started {
+                    started = true
+                    self.bfs(tile)
+                } else {
+                    self.bfs(nil)
+                }
             }
         }
-
+        
     }
     
     override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
         if let touch = touches.anyObject() as? UITouch {
             let fadeIn = SKAction.fadeInWithDuration(0)
-
             
-            if let shapeNode = nodeAtPoint(touch.locationInNode(self)) as? SKShapeNode {
+            
+            if let shapeNode = nodeAtPoint(touch.locationInNode(self)) as? Tile {
                 shapeNode.runAction(fadeIn)
             }
         }
---
+++ graphik/MenuScene.swift
@@ -0,0 +1,17 @@
+//
+//  MenuScene.swift
+//  graphik
+//
+//  Created by Dostalova, Zuzana on 08/03/15.
+//  Copyright (c) 2015 zuzana.dostalova. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class MenuScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        
+    }
+    
+}
